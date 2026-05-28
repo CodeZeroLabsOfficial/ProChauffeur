8fd04d169aa91d66006cb56567975f57bda3bd02
Daniel Erdos <dev@codezerolabs.com>
2026-05-28 11:53:50 +1000

    Removed text from the sign-in layout

--- src/app/(full-width-pages)/(auth)/layout.tsx
+++ src/app/(full-width-pages)/(auth)/layout.tsx
@@ -21,12 +21,9 @@ export default function AuthLayout({
               {/* <!-- ===== Common Grid Shape Start ===== --> */}
               <GridShape />
               <div className="flex flex-col items-center max-w-xs">
-                <Link href="/" className="block mb-4">
+                <Link href="/" className="block">
                   <BrandingLogo asset="authLogo" width={231} height={48} />
                 </Link>
-                <p className="text-center text-gray-400 dark:text-white/60">
-                  Free and Open-Source Tailwind CSS Admin Dashboard Template
-                </p>
               </div>
             </div>
           </div>
